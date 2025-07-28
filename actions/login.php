27623eac6a1515c9e80713669e26d63302bf7c47
asanka0822 <asanka0822edu@gmail.com>
2025-07-28 19:40:45 +0530

    update index and login

--- actions/login.php
+++ actions/login.php
@@ -33,13 +33,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $redirectPage = '';
             switch ($_SESSION['role']) {
                 case 'admin':
-                    $redirectPage = '/mini%203ii/test2/views/admin/dashboard.php';
+                    $redirectPage = '/views/admin/dashboard.php';
                     break;
                 case 'teacher':
-                    $redirectPage = '/mini%203ii/test2/views/teacher/dashboard.php';
+                    $redirectPage = '/views/teacher/dashboard.php';
                     break;
                 case 'student':
-                    $redirectPage = '/mini%203ii/test2/views/student/dashboard.php';
+                    $redirectPage = '/views/student/dashboard.php';
                     break;
             }
             header("Location: $redirectPage");
