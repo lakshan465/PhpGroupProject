6a7e7599fe153d5a677283f9a5d946295769bfb9
asanka0822 <asanka0822edu@gmail.com>
2025-08-02 10:42:06 +0530

    pdf create required items

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'quiz-system/php-group-project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4c34308f9dbf96daf53250ed9d1683234259713f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'quiz-system/php-group-project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4c34308f9dbf96daf53250ed9d1683234259713f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'ca5b6de294512145db96bcbc94e61696599c391d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
